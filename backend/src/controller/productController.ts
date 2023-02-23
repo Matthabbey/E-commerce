20e78316ff0f0c516819f694de8793e1abc339ce
Akinpelu Abiodun <59386529+Matthabbey@users.noreply.github.com>
2023-02-23 22:51:04 +0100

    pagination,sorting,limit

--- backend/src/controller/productController.ts
+++ backend/src/controller/productController.ts
@@ -1,93 +1,133 @@
 import express, { Request, Response } from "express";
 import { ProductModel } from "../models/ProductModel";
-import slugify from 'slugify'
+import slugify from "slugify";
 import { validateMongoId } from "../utilities/utils";
 
-
-
-export const CreateProduct = async(req: Request, res: Response)=>{
-    try {
-        if(req.body.title){
-            req.body.slug = slugify(req.body.title)
-        }
-        const newProduct = await ProductModel.create(req.body)
-        return res.status(200).json({product: newProduct})
-    } catch (error) {
-        res.status(500).json({
-            Error: `Internal server ${error}`,
-            route: "/product/create"
-    })
-}
+export const CreateProduct = async (req: Request, res: Response) => {
+  try {
+    if (req.body.title) {
+      req.body.slug = slugify(req.body.title);
+    }
+    const newProduct = await ProductModel.create(req.body);
+    return res.status(200).json({ product: newProduct });
+  } catch (error) {
+    res.status(500).json({
+      Error: `Internal server ${error}`,
+      route: "/product/create",
+    });
+  }
 };
 
 export const GetAllProducts = async (req: Request, res: Response) => {
-    try {
-      //Request dot Query(req.query) is use to sort, filter or cause a limit of views to what you want to see in the getAll http method.
-      const products = await ProductModel.find();
-      return res.status(200).json({
-        message: "You have successfully retrieved all products in your database",
-        Products: products,
-      });
-    } catch (error) {
-        res.status(500).json({
-            Error: `Internal server ${error}`,
-            route: "/product/get-all"
-    })
-    }
-  };
+  try {
+    // Filter
+    const queryObj = { ...req.query };
+    const excludeField = ["page", "sort", "limit", "fields"];
+    let m = excludeField.forEach((el) => delete queryObj[el]); //Not in used
+    let queryStr = JSON.stringify(queryObj);
+    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
 
-  export const GetSingleProduct = async (req: Request, res: Response) => {
-    try {
-      //Request dot Query(req.query) is use to sort, filter or cause a limit of views to what you want to see in the getAll http method.
-      const product = await ProductModel.findById(req.params.id);
-      return res.status(200).json({
-        message: "You have successfully retrieved all product in your database",
-        Product: product,
-      });
-    } catch (error) {
-      res.status(500).json({
-        Error: `Internal server ${error}`,
-        route: "/product/get-single"
-      });
-    }
-  };
+    let query = ProductModel.find(JSON.parse(queryStr));
 
-  export const UpdateProduct = async (req: Request, res: Response) => {
-    const id = req.params.id;
-    try {
-        if(req.body.title){
-            req.body.slug = slugify(req.body.title)
+    //Sort
+    if(req.query.sort){
+        const sortBy = req.query.sort.split(',').join(' ')
+        query = query.sort(sortBy)
+    }else{
+        query = query.sort("-createdAt")
+    }
+    //Limiting the showing fields
+    if(req.query.fields){
+        const fields = req.query.fields.split(',').join(' ')
+        query = query.select(fields)
+
+    }else{
+        query = query.select("-__v")
+
+    }
+    //Pagination
+    const page: any = req.query.page
+    const limit: any = req.query.limit
+    const skip = (page - 1) * limit
+    query = query.skip(skip).limit(limit)
+    if(req.query.page){
+        const productCount = await ProductModel.countDocuments()
+        if(skip >= productCount){
+           return res.status(404).json({msg: "Sorry, This page does not exits"})
         }
-      const update = await ProductModel.findByIdAndUpdate(id, req.body, {new: true});
-      return res.status(200).json({
-        message: "Successfully updated",
-        Updated: update
-      });
-    } catch (error) {
-      res.status(500).json({
-        Error: `Internal server ${error}`,
-        route: "/product/update-product"
+        
+    }
+    console.log(page, limit);
+    
+
+    const products = await query;
+    return res.status(200).json({
+      message: "You have successfully retrieved all products in your database",
+      Products: products,
+    });
+  } catch (error) {
+    res.status(500).json({
+      Error: `Internal server ${error}`,
+      route: "/product/get-all",
+    });
+  }
+};
+
+export const GetSingleProduct = async (req: Request, res: Response) => {
+  try {
+    //Request dot Query(req.query) is use to sort, filter or cause a limit of views to what you want to see in the getAll http method.
+    const product = await ProductModel.findById(req.params.id);
+    return res.status(200).json({
+      message: "You have successfully retrieved all product in your database",
+      Product: product,
+    });
+  } catch (error) {
+    res.status(500).json({
+      Error: `Internal server ${error}`,
+      route: "/product/get-single",
+    });
+  }
+};
+
+export const UpdateProduct = async (req: Request, res: Response) => {
+  const id = req.params.id;
+  try {
+    if (req.body.title) {
+      req.body.slug = slugify(req.body.title);
+    }
+    const update = await ProductModel.findByIdAndUpdate(id, req.body, {
+      new: true,
+    });
+    return res.status(200).json({
+      message: "Successfully updated",
+      Updated: update,
+    });
+  } catch (error) {
+    res.status(500).json({
+      Error: `Internal server ${error}`,
+      route: "/product/update-product",
+    });
+  }
+};
+
+export const DeleteProduct = async (req: Request, res: Response) => {
+  const id = req.params.id;
+  validateMongoId(id);
+  try {
+    const deleteMe = await ProductModel.findByIdAndDelete(id);
+
+    if (!deleteMe) {
+      return res.status(404).json({
+        message: "This item has been deleted",
       });
     }
-  };
-  export const DeleteProduct = async (req: Request, res: Response) => {
-    const id = req.params.id;
-    validateMongoId(id);
-    try {
-      const deleteMe = await ProductModel.findByIdAndDelete(id);
-  
-      if (!deleteMe) {
-        return res.status(404).json({
-          message: "This item has been deleted",
-        });
-      }
-      return res.status(200).json({
-        message: "Successfully deleted",
-      });
-    } catch (error) {
-      res.status(500).json({
-        Error: `Internal server ${error}`,
-        route: "/product/delete-product"
-      });
-    }
-  };
+    return res.status(200).json({
+      message: "Successfully deleted",
+    });
+  } catch (error) {
+    res.status(500).json({
+      Error: `Internal server ${error}`,
+      route: "/product/delete-product",
+    });
+  }
+};
