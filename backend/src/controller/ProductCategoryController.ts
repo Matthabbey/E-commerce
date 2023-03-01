b858059e880de59690ccb68a5d3d8b06a19aa80e
Akinpelu Abiodun <59386529+Matthabbey@users.noreply.github.com>
2023-03-01 23:51:50 +0100

    crud for product

--- backend/src/controller/ProductCategoryController.ts
+++ backend/src/controller/ProductCategoryController.ts
@@ -6,11 +6,79 @@ import { validateMongoId } from "../utilities/utils";
 export const CreateProductCategory = async (req: Request, res: Response) => {
   try {
     const productCategory = await ProductCategoryModel.create(req.body);
-    return res.status(200).json({ cat: productCategory });
+    return res.status(200).json({message: "Successfully Created Product Category",cat: productCategory });
   } catch (error) {
     res.status(500).json({
       Error: `Internal server ${error}`,
       route: "/category/create",
+    });
+  }
+};
+ export const UpdateProductCategory = async (req: Request, res: Response) => {
+  try {
+    const { id } = req.params
+    validateMongoId(id)
+    const updateCategory =  await ProductCategoryModel.findByIdAndUpdate(id, req.body, {new: true})
+    return res.status(200).json({ message:"Successfully Updated Category", update: updateCategory });
+
+  } catch (error) {
+    res.status(500).json({
+      Error: `Internal server ${error}`,
+      route: "/category/create",
+    });
+  }
+ }
+
+ export const DeleteProductCategory = async (req: Request, res: Response) => {
+  const id = req.params.id;
+  validateMongoId(id);
+  try {
+    const deleteMe = await ProductCategoryModel.findByIdAndDelete(id);
+
+    if (!deleteMe) {
+      return res.status(404).json({
+        message: "This item has been deleted",
+      });
+    }
+    return res.status(200).json({
+      message: "Successfully deleted",
+    });
+  } catch (error) {
+    res.status(500).json({
+      Error: `Internal Server ${error}`,
+      route: "user/delete router",
+    });
+  }
+};
+
+export const GetSingleProductCategory = async (req: Request, res: Response) => {
+  try {
+    //Request dot Query(req.query) is use to sort, filter or cause a limit of views to what you want to see in the getAll http method.
+    const productcategory = await ProductCategoryModel.findById(req.params.id);
+    return res.status(200).json({
+      message: "You have successfully retrieved all productcategory in your database",
+      Product: productcategory,
+    });
+  } catch (error) {
+    res.status(500).json({
+      Error: `Internal server ${error}`,
+      route: "/productcategory/get-single",
+    });
+  }
+};
+
+export const GetAllProductCategory = async (req: Request, res: Response) => {
+  try {
+    //Request dot Query(req.query) is use to sort, filter or cause a limit of views to what you want to see in the getAll http method.
+    const productcategory = await ProductCategoryModel.find();
+    return res.status(200).json({
+      message: "You have successfully retrieved all productcategory in your database",
+      product: productcategory,
+    });
+  } catch (err) {
+    res.status(500).json({
+      Error: `Internal Server ${err}`,
+      route: "productcategory/get-all-productcategory",
     });
   }
 };
--- backend/src/routes/ProductCategoryRoute.ts
+++ backend/src/routes/ProductCategoryRoute.ts
@@ -1,8 +1,13 @@
 import express from 'express';
-import { CreateCategory } from '../controller/ProductCategoryController';
+import { CreateProductCategory, DeleteProductCategory, UpdateProductCategory, GetAllProductCategory, GetSingleProductCategory } from '../controller/ProductCategoryController';
 import { authMiddleware, isAdmin } from '../middlewares/authRoutes';
 const router = express.Router();
 
-router.post('/create', authMiddleware, isAdmin, CreateCategory)
+router.post('/create', authMiddleware, isAdmin, CreateProductCategory)
+router.put('/update/:id', authMiddleware, isAdmin, UpdateProductCategory)
+router.delete('/delete/:id', authMiddleware, isAdmin, DeleteProductCategory)
+router.get('/getsingle/:id', GetSingleProductCategory)
+router.get('/getall', GetAllProductCategory)
+
 
 export default router;
