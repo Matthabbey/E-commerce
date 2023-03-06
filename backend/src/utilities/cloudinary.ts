8c35a2a0faf7714e585a7d2edda1a9c76fa25bd7
Akinpelu Abiodun <59386529+Matthabbey@users.noreply.github.com>
2023-03-06 22:45:37 +0100

    sharp for image resizing

--- backend/src/utilities/cloudinary.ts
+++ backend/src/utilities/cloudinary.ts
@@ -1,21 +1,37 @@
-import multer from 'multer'
+import multer from "multer";
+import { CloudinaryStorage } from "multer-storage-cloudinary";
 
-const cloudinary = require('cloudinary').v2
-import {CloudinaryStorage} from 'multer-storage-cloudinary'
+const cloudinary = require("cloudinary").v2;
 
 cloudinary.config({
-    cloud_name:process.env.CLOUDINARY_CLOUD_NAME,
-    api_key:process.env.CLOUDINARY_API_KEY,
-    api_secret:process.env.CLOUDINARY_API_SECRET
-})
-
-const storage = new CloudinaryStorage({
-    cloudinary,
-    params: async(req, file)=>{
-        return {
-            folder: "E-COMMERCE"
-        }
-    }
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
-export const upload = multer({storage: storage})
+interface file{
+fileToUpload: string
+}
+
+export const cloudinaryUploadImage = async(fileToUpload: file)=>{
+    return new Promise((resolve)=>{
+        cloudinary.uploader.upload(fileToUpload, (result: any)=>{
+            resolve(
+                {
+                    url: result.secure._url
+                }
+            )
+        })
+    })
+}
+
+// const storage = new CloudinaryStorage({
+//   cloudinary,
+//   params: async (req, file) => {
+//     return {
+//       folder: "E-COMMERCE",
+//     };
+//   },
+// });
+
+// export const upload = multer({ storage: storage });
